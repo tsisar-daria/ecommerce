7d0673f360c5763c7f50f6403eed0efb0e48835f
Tsisar Daria <cysar.dasha@gmail.com>
2024-06-20 23:13:07 +0300

    commit 6

--- ECommerce.Api.Orders/obj/Debug/net8.0/ECommerce.Api.Orders.AssemblyInfo.cs
+++ ECommerce.Api.Orders/obj/Debug/net8.0/ECommerce.Api.Orders.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ECommerce.Api.Orders")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+35c27597af3903177d6d537abb44ea05402918ff")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bce959cb27fbe0bc38efd35ef2334124a4c236ba")]
 [assembly: System.Reflection.AssemblyProductAttribute("ECommerce.Api.Orders")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ECommerce.Api.Orders")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
